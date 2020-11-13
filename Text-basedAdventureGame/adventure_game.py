0efc1f52af4cc7bb674cab8db6056dc3cc2eeb7d
Ruben <rubenzurr@gmail.com>
2020-11-13 19:21:58 +0300

    minor improvements, still a lot to fix

--- Text-basedAdventureGame/adventure_game.py
+++ Text-basedAdventureGame/adventure_game.py
@@ -1,18 +1,18 @@
-# Текстовое приключение состоит из ВИДОВ (например: комната, в которой есть различные предметы, взаимодействующие между
-# собой. Внутри комнат также можно фокусироваться на предметах/примечательных объектах
+# Text adventure состоит из комнат (например: комната, в которой есть различные предметы, с которыми можно
+# взаимодействовать. В одной из комнат точно должна быть одна SpecialExitDoor, закрытая ключом и заканчивающая игру.
 import random as rnd
 
-# class InteractableObject: # Need to classify which items are interactable and which are part of room (window/door/etc)
+
 n_of_rooms = 3
+n_of_doors = n_of_rooms - 1
 n_of_max_items = 3
 n_of_keys = 1
-# n_of_doors = n_of_rooms - 1
 
 
 class Item:
     """ Main ITEM class """
     item_id = 0  # Each generated item has ID
-    item_size_list = ['tiny', 'small', 'medium', 'large', 'enormous']
+    item_size_list = ['tiny', 'small', 'medium', 'large', 'enormous']  # Наверное надо держать эти листы вне класса
     durability_list = ['fragile', 'very durable', 'sturdy', 'flexible', 'breakable']
     quality = ['new', 'used', 'terrible', 'broken']
     material = ['wooden', 'metallic', 'plastic', 'fiber', 'stone']
@@ -21,10 +21,11 @@ class Item:
         Item.item_id += 1
         self.item_size = rnd.choice(self.item_size_list)
         self.durability = rnd.choice(self.durability_list)
-        self.item_name = __class__.__name__
+        self.item_name = self.__class__.__name__
+        self.item_generated()
 
     def item_generated(self):
-        print(f"Generated {__class__.__name__}, item id: {self.item_id}")
+        print(f"Generated {self.item_name}, item id: {self.item_id}")
 
 
 class Chair(Item):
@@ -72,11 +73,19 @@ class Key(Item):
 
 
 class Door:
-    door_id = 0
     """ Door between rooms, there is at least 1 door between 2 rooms """
+    door_id = 0
 
     def __init__(self):
         Door.door_id += 1
+        print(f"Generated {__class__.__name__.lower()}, door id: {self.door_id}")
+
+
+class UsualDoor(Door):
+    """ Door between rooms, there is at least 1 door between 2 rooms """
+
+    def __init__(self):
+        super().__init__()
         print(f"Generated {__class__.__name__.lower()}, door id: {self.door_id}")
 
 
@@ -96,16 +105,17 @@ class SpecialExitDoor(Door):
 
 
 class Room:
-    """ Game consists of Rooms between which player can walk and interact with random items """
+    """ Game consists of Rooms between which player can walk and interact with items """
     room_id = 0
-    room_type = ['dungeon', 'apartment', 'closet', 'corridor']
+    room_type = ['dungeon', 'apartment', 'closet', 'corridor']  # Can later make as subclasses to add variety
     illumination_type = ['blindingly bright', 'dark', 'dusk', 'dim', 'very dark', 'barely lit']
     environment_type = ['lovecraft-ish', 'hi-tech', 'urban', 'filthy', 'sterile']
     items_list = [Pen, Bottle, Gun, Chair, Key]  # List of all possible items
-    door_list = [Door, SpecialExitDoor]
+    door_list = [UsualDoor, SpecialExitDoor]
 
     def __init__(self):
         Room.room_id += 1
+        self.room = Room.room_id
         self.room_type = rnd.choice(self.room_type)
         self.illumination = rnd.choice(self.illumination_type)
         self.env = rnd.choice(self.environment_type)
@@ -113,16 +123,18 @@ class Room:
         self.actual_items = []
         self.contains_key = False
         self.player_in_room = False
+
         self.generate_room_filling(n_of_max_items)
         self.generate_room_doors()
 
     def generate_room_filling(self, max_items):
-        """ generates ITEMS which are in 'items_in_room' list"""
+        """ generates ITEMS in room you entered, which are in 'items_in_room' list"""
         for item in range(rnd.randint(0, max_items)):
             self.items_in_room.append(rnd.choice(self.items_list))  # Storing list of items for each room
         print(f"Generating room #{self.room_id}:")
         print(f"There are {len(self.items_in_room)} items")
         print(f"Room seems to be {self.room_type}, which is {self.illumination} and looks {self.env}. You can also see next items:")
+
         # Adding items to item list
         for item in range(len(self.items_in_room)):  # Actual creation of items which were in list of items
             print(f'{item + 1}) ', end='')
@@ -130,6 +142,8 @@ class Room:
         # Adding doors to item list
 
     def generate_room_doors(self):
+        """ generates DOORS in room you entered """
+        self.actual_items.append(SpecialExitDoor())
         if (len(game.list_of_rooms) + 1) < n_of_rooms:
             doors_in_room = rnd.choice(range(1, 3))
             game.n_of_doors -= doors_in_room
@@ -142,12 +156,11 @@ class Room:
     def room_contains_key(self):
         """ Check if room has key """
         if Key in self.items_in_room:
-            self.contains_key = True
-            return self.contains_key
+            return True
 
 
 class GameManager:
-
+    """ Здесь происходит весь процесс игры: начало, конец, обработка команд """
     def __init__(self, n_of_rooms):
         self.list_of_rooms = []  # Maybe add 'Beginning room' (to fill index 0)
         self.player_has_key = False
@@ -160,54 +173,49 @@ class GameManager:
 
     def start_game(self):
         """ Generates rooms and SHOULD PLACE PLAYER in one of them """
-        # for room in self.list_of_rooms:  # This should only work if there is 1 room. (refactor to work with many rooms)
-        game.list_of_rooms.append(Room())
+        game.list_of_rooms.append(Room())  # Пока сделано для одной комнаты
         start_room = self.list_of_rooms[0]
         self.current_room_key = start_room.room_contains_key()
-        # self.special_door_unlocked = start_room.actual_items[0].is_unlocked
-            # if self.special_door_unlocked is None:
-            #     self.special_door_unlocked = only_room.actual_items[0].is_unlocked
+        print(self.current_room_key)
 
     def place_player(self):
         """ chooses in which room player starts """
         self.start_room = self.list_of_rooms.index((rnd.choice(self.list_of_rooms)))
         print(f'\nPlayer is in a room #{self.start_room + 1}')
 
-    def input_handler(self):
+    def input_handler(self):  # Много повторений, наверняка есть более простой способ
+        """ Обработка команд игрока """
         plr_input = str(input()).lower().split()
-        if len(plr_input) >= 2:
-            if plr_input[0] == 'take':
-                if plr_input[1] == 'key':
-                    if self.current_room_key:
-                        self.player_has_key = True
-                        print('You picked up a key')
-                    else:
-                        print('There is no key in a room')
-                else:
-                    print('There is no such item in a room')
-            elif plr_input[0] == 'unlock':
-                if plr_input[1] == 'specialdoor':
-                    if self.player_has_key:
-                        self.special_door_unlocked = True
-                        print('You have unlocked the door')
-                    else:
-                        print("You don't have a key")
-                else:
-                    print(f"There is no {plr_input[1]} in a room")
-            elif plr_input[0] == 'enter':
-                if plr_input[1] == 'specialdoor':
-                    if self.special_door_unlocked:
-                        print('You finished the game 8)')
-                        self.game_is_running = False
-                    else:
-                        print("You don't have a key")
-                elif plr_input[1] == 'door':
-                    self.enter_new_door()
-                else:
-                    print(f"You can't enter {plr_input[1]}")
-            else:
-                print('There is no such command')
+        if len(plr_input) < 2:
+            return
 
+        if plr_input[0] == 'take':
+            if plr_input[1] == 'key':
+                self.take_key()
+            else:
+                print('There is no such item in a room')
+        elif plr_input[0] == 'unlock':
+            if plr_input[1] == 'specialdoor':
+                if self.player_has_key:
+                    self.special_door_unlocked = True
+                    print('You have unlocked the door')
+                else:
+                    print("You don't have a key")
+            else:
+                print(f"There is no {plr_input[1]} in a room")
+        elif plr_input[0] == 'enter':
+            if plr_input[1] == 'specialdoor':
+                if self.special_door_unlocked:
+                    print('You finished the game 8)')
+                    self.game_is_running = False
+                else:
+                    print("You don't have a key")
+            elif plr_input[1] == 'door':
+                self.enter_new_door()
+            else:
+                print(f"You can't enter {plr_input[1]}")
+        else:
+            print('There is no such command')
 
     def game_process(self):
         """ Main process where whole game is happening"""
@@ -217,9 +225,15 @@ class GameManager:
             self.input_handler()
 
     def enter_new_door(self):
-        self.list_of_rooms.append(Room())
-        print(f'You entered new room {len(self.list_of_rooms)}')
+        self.new_room = Room()
+        self.list_of_rooms.append(self.new_room)
+        print(f'You entered new room {len(self.list_of_rooms)} with id {self.list_of_rooms.index(self.new_room)}')
 
+    def take_key(self):
+        if self.current_room_key:
+            self.player_has_key = True
+            current_room.current_room_key = False
+            print('Player took the key')
 
 
 if __name__ == '__main__':
@@ -227,14 +241,10 @@ if __name__ == '__main__':
         start = str(input('Start new game? (Y/N): ')).lower()
         if start == 'y':
             game = GameManager(n_of_rooms)
+            print(f'{n_of_rooms} rooms in this adventure')
             game.game_process()
         elif start == 'n':
             break
         else:
             print('Wrong input. Try again')
             continue
-
-
-# Вопросы:
-# 1) Как лучше принимать инпут? Прописывать все циклы/условия выглядит излишне сложно
-# 2)
